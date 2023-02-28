119d1270fca59b43a4497af07dc2e7625b04d074
Renato Dias <renatocardosodias@icloud.com>
2023-02-28 00:10:45 -0300

    Criando o projeto inicial

---
+++ Weather/Weather/WeatherService.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherService.swift
+//  Weather
+//
+//  Created by Renato on 28/02/23.
+//
+
+import Foundation
+
+struct APIWeather: Decodable {
+    let description: String
+    
+} //end struct APIWeater
