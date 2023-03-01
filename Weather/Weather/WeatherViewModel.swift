f20a1f3844ca05db6f93dd0ff335fadac0585a63
Renato Dias <renatocardosodias@icloud.com>
2023-03-01 19:28:00 -0300

    Vamos pegar os dados no nosso serviços e converte ele em modelo para informação que nossa visualização precisa para mostrar no nosso aplicativo

---
+++ Weather/Weather/WeatherViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  WeatherViewModel.swift
+//  Weather
+//
+//  Created by Renato on 01/03/23.
+//
+// Vamos pegar os dados no nosso serviços e converte ele em modelo para informação que nossa visualização precisa para mostrar no nosso aplicativo
+import Foundation
+
+public class WeaterViewModel: ObservableObject {
+
+}
